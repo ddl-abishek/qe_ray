660b8dfd76dbfaf637e36e84ea8939cc201de0d4
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2024-02-18 15:20:55 -0800

    Pushed from Domino: https://dspftr3a37563.workbench-analyze-team-sandbox.domino.tech/workspace/integration-test/qe_ray?executionId=65d2793834aace69262129aa

--- .Trash-12574/files/ray_gpu_training.py
+++ .Trash-12574/files/ray_gpu_training.py
@@ -1,15 +1,34 @@
+import argparse
+from typing import Dict
+from ray.air import session
+import os
+
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-from ray.air import Checkpoint
+
+import ray.train as train
 from ray.train.torch import TorchTrainer
 from ray.air.config import ScalingConfig
-import ray.train as train
-from ray.air import session
-import argparse
-import os
+
+# Download training data from open datasets.
+training_data = datasets.FashionMNIST(
+    root="~/data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="~/data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
 
 # Define model
 class NeuralNetwork(nn.Module):
@@ -30,13 +49,11 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
-def train_epoch(dataloader, model, loss_fn, optimizer, device='cuda'):
-    size = len(dataloader.dataset) // session.get_world_size()  # Divide by word size
+
+def train_epoch(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset) // session.get_world_size()
     model.train()
     for batch, (X, y) in enumerate(dataloader):
-        # We don't need this anymore! Ray Train does this automatically:
-        X, y = X.to(device), y.to(device)  
-
         # Compute prediction error
         pred = model(X)
         loss = loss_fn(pred, y)
@@ -48,74 +65,55 @@ def train_epoch(dataloader, model, loss_fn, optimizer, device='cuda'):
 
         if batch % 100 == 0:
             loss, current = loss.item(), batch * len(X)
-            # print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
-def test_epoch(dataloader, model, loss_fn, device='cuda'):
-    size = len(dataloader.dataset) // session.get_world_size()  # Divide by word size
+
+def validate_epoch(dataloader, model, loss_fn):
+    size = len(dataloader.dataset) // session.get_world_size()
     num_batches = len(dataloader)
     model.eval()
     test_loss, correct = 0, 0
     with torch.no_grad():
         for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
     test_loss /= num_batches
     correct /= size
-    # print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(
+        f"Test Error: \n "
+        f"Accuracy: {(100 * correct):>0.1f}%, "
+        f"Avg loss: {test_loss:>8f} \n"
+    )
     return test_loss
 
-def load_data():
-    # Download training data from open datasets.
-    training_data = datasets.FashionMNIST(
-        root="data",
-        train=True,
-        download=True,
-        transform=ToTensor(),
-    )
 
-    # Download test data from open datasets.
-    test_data = datasets.FashionMNIST(
-        root="data",
-        train=False,
-        download=True,
-        transform=ToTensor(),
-    )
-    return training_data, test_data
-
-
-def train_func(config: dict):
+def train_func(config: Dict):
     batch_size = config["batch_size"]
     lr = config["lr"]
     epochs = config["epochs"]
-    
-    batch_size_per_worker = batch_size // session.get_world_size()
-    
-    training_data, test_data = load_data()  # <- this is new!
-    
+
+    worker_batch_size = batch_size // session.get_world_size()
+
     # Create data loaders.
-    train_dataloader = DataLoader(training_data, batch_size=batch_size_per_worker)
-    test_dataloader = DataLoader(test_data, batch_size=batch_size_per_worker)
-    
+    train_dataloader = DataLoader(training_data, batch_size=worker_batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=worker_batch_size)
+
     train_dataloader = train.torch.prepare_data_loader(train_dataloader)
     test_dataloader = train.torch.prepare_data_loader(test_dataloader)
-    
+
+    # Create model.
     model = NeuralNetwork()
     model = train.torch.prepare_model(model)
-    
+
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=lr)
-    
-    for t in range(epochs):
-        train_epoch(train_dataloader, model, loss_fn, optimizer)
-        test_loss = test_epoch(test_dataloader, model, loss_fn)
-        checkpoint = Checkpoint.from_dict(
-            dict(epoch=t, model=model.state_dict())
-        )
-        session.report(dict(loss=test_loss), checkpoint=checkpoint)
 
-    print("Done!")
+    for _ in range(epochs):
+        train_epoch(train_dataloader, model, loss_fn, optimizer)
+        loss = validate_epoch(test_dataloader, model, loss_fn)
+        session.report(dict(loss=loss))
+
 
 def train_fashion_mnist(num_workers=3, use_gpu=True):
     trainer = TorchTrainer(
@@ -128,7 +126,8 @@ def train_fashion_mnist(num_workers=3, use_gpu=True):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num-workers","-n", type=int, default=3, help="Sets number of workers for training.",)
+    parser.add_argument("--address", required=False, type=str, help="the address to use for Ray")
+    parser.add_argument("--num-workers","-n", type=int, default=2, help="Sets number of workers for training.",)
     parser.add_argument("--use-gpu", action="store_true", default=True, help="Enables GPU training")
     args, _ = parser.parse_known_args()
 
@@ -139,4 +138,4 @@ if __name__ == "__main__":
         service_port = os.environ["RAY_HEAD_SERVICE_PORT"]
         ray.util.connect(f"{service_host}:{service_port}")
 
-    train_fashion_mnist(num_workers=3, use_gpu=True)
+    train_fashion_mnist(num_workers=args.num_workers, use_gpu=True)
